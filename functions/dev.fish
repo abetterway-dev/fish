086a6ad54e1d4c8a9e2e4689a58b5423302143cd
Conrad <conrad@getmore.digital>
2025-08-26 23:05:34 -0600

    fixed error in dev

--- functions/dev.fish
+++ functions/dev.fish
@@ -9,6 +9,7 @@ function dev --description 'Open ~/Developer/<folder> with PROGRAM (default: ran
 
     set -l program $ide
     set -l base_dir $dev_dir
+    set -l folder
 
     if test (count $argv) -eq 0
         true
@@ -23,12 +24,9 @@ function dev --description 'Open ~/Developer/<folder> with PROGRAM (default: ran
         set folder $argv[2]
     end
 
-    set -l dir "$base_dir/$folder"
-
     # run $program with kotlin/swift guard
-    if test -n "$folder" -a -d "$dir"
-        cd $dir
-        run $program -on "$dir" -g
+    if test -d "$base_dir/$folder"
+        run $program -on "$folder" -g
     else
         run $program $extra --from $base_dir -g
     end
